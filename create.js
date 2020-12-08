1641008a4530dba736408b987141ec7a45d97c44
Charles <csampson@westech-inc.com>
2020-12-08 11:16:47 -0700

    Adding Serverless API

--- create.js
+++ create.js
@@ -1,43 +1,23 @@
 import * as uuid from "uuid";
-import AWS from 'aws-sdk';
+import handler from './libs/handler-lib';
+import dynamoDb from './libs/dynamodb-lib';
+// import AWS from 'aws-sdk';
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+// const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-export function main(event,context,callback) {
+export const main = handler( async (event,context) => {
   const data = JSON.parse(event.body);
-
   const params = {
     TableName: process.env.tableName,
     Item: {
-      userId: event.requestContext.identity.cognitoIdentityId,
+      userId: '123',//event.requestContext.identity.cognitoIdentityId,
       noteId: uuid.v1(),
       content: data.content,
       attachment: data.attachment,
       createdAt: Date.now()
-    }
+    },
   };
 
-  dynamoDb.put(params, (error,data) => {
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true
-    };
-
-    if (error) {
-      console.log("---ERROR---",error);
-      const response = {
-        statusCode: 500,
-        headers: headers,
-        body: JSON.stringify({ status: false })
-      };
-      callback(null, response);
-      return;
-    }
-    const response = {
-      statusCode: 200,
-      headers: headers,
-      body: JSON.stringify(params.Item)
-    };
-    callback(null, response);
-  });
-}
+  await dynamoDb.put(params);
+  return params.Item;
+});
